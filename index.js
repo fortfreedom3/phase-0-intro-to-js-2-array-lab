0ba6b4a4654f398bf77940f6132df1ae8c52e040
Morgan Byrne <morganlbyrne@gmail.com>
2021-10-07 11:28:54 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+};
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+    return cats.pop()
+}
+
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift(name);
+}
+
+function appendCat(name) {
+    const catCopy = [...cats, name];
+    return catCopy;
+}
+
+function prependCat(name) {
+    const catCopy = [name, ...cats];
+    return catCopy;
+}
+
+function removeLastCat(name) {
+    return cats.slice(0, -1);
+}
+
+function removeFirstCat(name) {
+    return cats.slice(1);
+}
+
